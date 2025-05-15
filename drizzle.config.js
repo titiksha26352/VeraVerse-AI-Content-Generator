3b18c8a71d7bbe51145b9d45cdeb00049608a853
Titiksha Tripathi <158046431+titiksha26352@users.noreply.github.com>
2025-05-15 21:02:23 +0530

    Add files via upload

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.tsx",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://neondb_owner:npg_K1QU2XStNLwp@ep-twilight-meadow-a4m1t4vt-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require',
+
+    }
+  };
+
+ 
+  
